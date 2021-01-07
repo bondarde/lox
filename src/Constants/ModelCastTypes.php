45f75f0dee4687e1ee6f99d1540534b11da705d9
Alexander Bondar <alexander@bondar.de>
2021-01-07 21:22:15 +0100

    Add constants for casts of model attributes

---
+++ src/Constants/ModelCastTypes.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace BondarDe\LaravelToolbox\Constants;
+
+interface ModelCastTypes
+{
+    const BOOLEAN = 'bool';
+    const INTEGER = 'int';
+    const FLOAT = 'real';
+    const DECIMAL = 'decimal';
+    const STRING = 'string';
+    const OBJECT = 'object';
+    const ARRAY = 'array';
+    const COLLECTION = 'collection';
+    const DATE = 'date';
+    const DATETIME = 'datetime';
+    const TIMESTAMP = 'timestamp';
+}
