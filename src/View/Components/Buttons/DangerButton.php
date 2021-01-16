f3600cd18c5d3bb5b64b5fcc6bdb6190982481e3
Alexander Bondar <alexander@bondar.de>
2021-01-17 00:15:26 +0100

    Add focus rings for buttons

--- src/View/Components/Buttons/DangerButton.php
+++ src/View/Components/Buttons/DangerButton.php
@@ -7,7 +7,7 @@ class DangerButton extends DefaultButton
     function makeAttributes(): array
     {
         return [
-            'class' => 'inline-flex items-center px-4 py-2 border border-transparent rounded-md font-semibold text-sm text-white uppercase tracking-widest focus:outline-none disabled:opacity-25 transition ease-in-out duration-150 bg-red-800 focus:ring-red-200 focus:border-red-900 hover:bg-red-700 active:bg-red-700',
+            'class' => 'inline-flex items-center px-4 py-2 border border-transparent rounded-md font-semibold text-sm text-white uppercase tracking-widest focus:outline-none disabled:opacity-25 transition ease-in-out duration-150 bg-red-800 focus:ring focus:ring-red-200 focus:border-red-900 hover:bg-red-700 active:bg-red-700',
         ];
     }
 }
--- src/View/Components/Buttons/SuccessButton.php
+++ src/View/Components/Buttons/SuccessButton.php
@@ -7,7 +7,7 @@ class SuccessButton extends DefaultButton
     function makeAttributes(): array
     {
         return [
-            'class' => 'inline-flex items-center px-4 py-2 border border-transparent rounded-md font-semibold text-sm text-white uppercase tracking-widest focus:outline-none disabled:opacity-25 transition ease-in-out duration-150 bg-green-700 focus:ring-green-200 focus:border-green-900 hover:bg-green-600 active:bg-green-800',
+            'class' => 'inline-flex items-center px-4 py-2 border border-transparent rounded-md font-semibold text-sm text-white uppercase tracking-widest focus:outline-none disabled:opacity-25 transition ease-in-out duration-150 bg-green-700 focus:ring focus:ring-green-100 focus:border-green-900 hover:bg-green-600 active:bg-green-800',
         ];
     }
 }
