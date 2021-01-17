cb489f3fc0fd6d4c9affc2d22a86309890fea202
Alexander Bondar <alexander@bondar.de>
2021-01-17 01:21:33 +0100

    UI polishing colors dark mode

--- src/View/Components/Buttons/DefaultButton.php
+++ src/View/Components/Buttons/DefaultButton.php
@@ -24,7 +24,7 @@ class DefaultButton extends Component implements Button
     function makeAttributes(): array
     {
         return [
-            'class' => 'inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-sm text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150',
+            'class' => 'inline-flex items-center px-4 py-2 border border-transparent rounded-md font-semibold text-sm text-white uppercase tracking-widest bg-gray-800 dark:bg-black hover:bg-gray-700 dark:hover:bg-gray-800 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150',
         ];
     }
 
--- src/View/Components/Buttons/LinkButton.php
+++ src/View/Components/Buttons/LinkButton.php
@@ -7,7 +7,7 @@ class LinkButton extends DefaultButton
     function makeAttributes(): array
     {
         return [
-            'class' => 'inline-flex items-center px-4 py-2 border border-transparent font-semibold text-sm uppercase tracking-widest rounded-md hover:bg-gray-200 active:bg-gray-300 focus:outline-none focus:border-gray-400 focus:ring focus:ring-gray-200 disabled:opacity-25 transition ease-in-out duration-150',
+            'class' => 'inline-flex items-center px-4 py-2 border border-transparent font-semibold text-sm uppercase tracking-widest rounded-md hover:bg-gray-200 dark:hover:bg-gray-700 active:bg-gray-300 focus:outline-none focus:border-gray-400 focus:ring focus:ring-gray-200 disabled:opacity-25 transition ease-in-out duration-150',
         ];
     }
 }
