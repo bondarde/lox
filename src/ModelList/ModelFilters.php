6e6149528dde13f9878abf1a36b4f9f2b32794ce
Alexander Bondar <alexander@bondar.de>
2021-12-01 15:21:07 +0100

    Add helpers for years and months filters

--- src/ModelList/ModelFilters.php
+++ src/ModelList/ModelFilters.php
@@ -2,6 +2,8 @@
 
 namespace BondarDe\LaravelToolbox\ModelList;
 
+use Carbon\Carbon;
+
 abstract class ModelFilters
 {
     const ALL = 'all';
@@ -13,5 +15,51 @@ abstract class ModelFilters
         return [
             self::ALL => new ModelFilter('alle', 'TRUE'),
         ];
+    }
+
+    public static function makeYearsFilter(string $fieldName, int $firstYear, ?callable $makeModelFilter = null, ?callable $makeFilterKey = null, ?int $lastYear = null): array
+    {
+        $filters = [];
+
+        $lastYear ??= Carbon::now()->year;
+        $makeModelFilter ??= function (int $y) use ($fieldName): ModelFilter {
+            return new ModelFilter(
+                $y,
+                'YEAR(' . $fieldName . ') = ' . $y,
+            );
+        };
+        $makeFilterKey ??= function (int $y): string {
+            return 'year-' . $y;
+        };
+
+        for ($y = $firstYear; $y <= $lastYear; $y++) {
+            $filters[$makeFilterKey($y)] = $makeModelFilter($y);
+        }
+
+        return $filters;
+    }
+
+    public static function makeMonthsFilter(string $fieldName, ?callable $makeModelFilter = null, ?callable $makeFilterKey = null): array
+    {
+        $filters = [];
+
+        $date = Carbon::now()->firstOfMonth();
+        $makeModelFilter ??= function (int $m) use ($date, $fieldName): ModelFilter {
+            $date->month($m);
+
+            return new ModelFilter(
+                $date->format('M'),
+                'MONTH(' . $fieldName . ') = ' . $m,
+            );
+        };
+        $makeFilterKey ??= function (int $y): string {
+            return 'month-' . $y;
+        };
+
+        for ($m = 1; $m <= 12; $m++) {
+            $filters[$makeFilterKey($m)] = $makeModelFilter($m);
+        }
+
+        return $filters;
     }
 }
