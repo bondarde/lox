a4083e09d10f806c640471ff54f8b3f6c24b2b25
Alexander Bondar <alexander@bondar.de>
2022-01-01 18:33:59 +0100

    Group password protected routes

--- routes/user.php
+++ routes/user.php
@@ -38,26 +38,20 @@ Route::group([
     Route::get('/user/profile/update-password', UserPasswordEditController::class)->name('user.profile.password.edit');
     Route::post('/user/profile/update-password', UserPasswordUpdateController::class)->name('user.profile.password.update');
 
-    Route::get('/user/profile/enable-second-factor', SecondFactorEnableStartController::class)->name('user.profile.second-factor.enable.start')
-        ->middleware([
+    Route::group([
+        'middleware' => [
             'password.confirm',
-        ]);
-    Route::post('/user/profile/enable-second-factor', SecondFactorEnableConfirmController::class)->name('user.profile.second-factor.enable.confirm')
-        ->middleware([
-            'password.confirm',
-        ]);
+        ],
+    ], function () {
+        Route::get('/user/profile/enable-second-factor', SecondFactorEnableStartController::class)->name('user.profile.second-factor.enable.start');
+        Route::post('/user/profile/enable-second-factor', SecondFactorEnableConfirmController::class)->name('user.profile.second-factor.enable.confirm');
 
-    Route::post('/user/profile/logout-other-browser-sessions', LogoutOtherBrowserSessionsController::class)->name('user.profile.logout-other-browser-sessions')
-        ->middleware([
-            'password.confirm',
-        ]);
+        Route::post('/user/profile/logout-other-browser-sessions', LogoutOtherBrowserSessionsController::class)->name('user.profile.logout-other-browser-sessions');
 
-    Route::get('/user/profile/delete-account', DeleteAccountConfirmationController::class)->name('user.profile.delete-account.confirm')->middleware([
-        'password.confirm',
-        'throttle:12,1',
-    ]);
-    Route::post('/user/profile/delete-account', DeleteAccountDeleteController::class)->name('user.profile.delete-account.delete')
-        ->middleware([
-            'password.confirm',
-        ]);
+        Route::get('/user/profile/delete-account', DeleteAccountConfirmationController::class)->name('user.profile.delete-account.confirm')
+            ->middleware([
+                'throttle:12,1',
+            ]);
+        Route::post('/user/profile/delete-account', DeleteAccountDeleteController::class)->name('user.profile.delete-account.delete');
+    });
 });
