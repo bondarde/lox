8cb5f80e8211e2ca1b891452f6a240ca1db047bd
Alexander Bondar <alexander@bondar.de>
2023-01-13 00:27:25 +0100

    Add explicit import

--- routes/local.php
+++ routes/local.php
@@ -1,5 +1,6 @@
 <?php
 
 use BondarDe\LaravelToolbox\Http\Controllers\Local\ViteAssetsFallbackController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('.build/.vite/assets/{file}', ViteAssetsFallbackController::class)->where('file', '.*');
