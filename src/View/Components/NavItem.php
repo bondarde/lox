f594650f760c2e426b4a46a8d3a6d53c9406b9c7
Alexander Bondar <alexander@bondar.de>
2023-03-27 01:58:56 +0200

    Allow nav highlight configuration

--- src/View/Components/NavItem.php
+++ src/View/Components/NavItem.php
@@ -19,9 +19,9 @@ class NavItem extends Component
         $active = Route::is($activeRoute);
 
         $baseClasses = 'relative after:absolute after:block after:h-1 after:-bottom-1 after:transition-all after:-skew-x-12';
-        $hoverClasses = 'hover:after:left-0 hover:after:w-full hover:after:bg-brand';
-        $inactiveClasses = 'after:left-1/2 after:w-0 after:bg-brand';
-        $activeClasses = 'after:w-full after:bg-brand/50';
+        $hoverClasses = 'hover:after:left-0 hover:after:w-full hover:after:bg-nav-highlight';
+        $inactiveClasses = 'after:left-1/2 after:w-0 after:bg-nav-highlight';
+        $activeClasses = 'after:w-full after:bg-nav-highlight/75';
 
         $this->cssClasses = collect([
             $baseClasses,
