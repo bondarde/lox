f8f0374563cf5bcdff7ab38dba66eb4e626e97e7
Alexander Bondar <alexander@bondar.de>
2023-08-09 20:19:29 +0200

    Use PHP 8.2 syntax

--- src/ModelListData.php
+++ src/ModelListData.php
@@ -2,32 +2,17 @@
 
 namespace BondarDe\Lox;
 
-class ModelListData
+readonly class ModelListData
 {
-    public string $model;
-    public int $activePage;
-    public array $allFilters;
-    public array $activeFilters;
-    public array $allSorts;
-    public array $activeSorts;
-    public ?string $searchQuery;
-
     public function __construct(
-        string  $model,
-        int     $activePage,
-        array   $allFilters,
-        array   $activeFilters,
-        array   $allSorts,
-        array   $activeSorts,
-        ?string $searchQuery = null,
+        public string  $model,
+        public int     $activePage,
+        public array   $allFilters,
+        public array   $activeFilters,
+        public array   $allSorts,
+        public array   $activeSorts,
+        public ?string $searchQuery = null,
     )
     {
-        $this->model = $model;
-        $this->activePage = $activePage;
-        $this->allFilters = $allFilters;
-        $this->activeFilters = $activeFilters;
-        $this->allSorts = $allSorts;
-        $this->activeSorts = $activeSorts;
-        $this->searchQuery = $searchQuery;
     }
 }
