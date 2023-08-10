29b39c2089be89a5ec69e73d95ec107aa9fe9d43
Alexander Bondar <alexander@bondar.de>
2023-08-10 12:05:18 +0200

    Extend docs

--- src/ModelList/ModelListFilterable.php
+++ src/ModelList/ModelListFilterable.php
@@ -4,5 +4,8 @@ namespace BondarDe\Lox\ModelList;
 
 interface ModelListFilterable
 {
+    /**
+     * @return ?string class returning {@link ModelFilters}
+     */
     public static function getModelListFilters(): ?string;
 }
--- src/ModelList/ModelListSortable.php
+++ src/ModelList/ModelListSortable.php
@@ -4,5 +4,8 @@ namespace BondarDe\Lox\ModelList;
 
 interface ModelListSortable
 {
+    /**
+     * @return ?string class returning {@link ModelSorts}
+     */
     public static function getModelListSorts(): ?string;
 }
