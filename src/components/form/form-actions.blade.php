0351ec8bcdd921fba82d6ca855126c78c76d68ea
Alexander Bondar <alexander@bondar.de>
2023-08-16 13:33:47 +0200

    Fix UI

--- src/components/form/form-actions.blade.php
+++ src/components/form/form-actions.blade.php
@@ -9,7 +9,7 @@
 <div class="md:flex p-4 mb-8 gap-8">
     <div class="mb-4 md:w-1/4">
     </div>
-    <div class="md:w-2/4">
+    <div class="md:w-3/4">
         {{ $cancel ?? '' }}
     </div>
 </div>
