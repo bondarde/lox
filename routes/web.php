206c78fb73416dd3cfebcf427d88c1ba0554db89
Alexander Bondar <alexander@bondar.de>
2023-08-18 13:01:07 +0200

    Disable CSRF token check for social login callbacks

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,15 @@
 use BondarDe\Lox\Http\Controllers\TinyMceFilesController;
 use BondarDe\Lox\Http\Controllers\Web\Sso\SsoCallbackController;
 use BondarDe\Lox\Http\Controllers\Web\Sso\SsoRedirectController;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Features;
 
 if (Features::enabled('sso')) {
     Route::group(['middleware' => ['web']], function () {
         Route::get('login/{provider}', SsoRedirectController::class)->name('sso.redirect');
-        Route::any('login/{provider}/callback', SsoCallbackController::class)->name('sso.callback');
+        Route::any('login/{provider}/callback', SsoCallbackController::class)->name('sso.callback')
+            ->withoutMiddleware(VerifyCsrfToken::class);
     });
 }
 Route::get('tinymce/{file}', TinyMceFilesController::class)->where('file', '.*');
