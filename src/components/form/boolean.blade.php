0806be98f07f12e1b4f263200374bf2a2bf6f4c4
Alexander Bondar <alexander@bondar.de>
2023-08-29 17:08:09 +0200

    UI polishing

--- src/components/form/boolean.blade.php
+++ src/components/form/boolean.blade.php
@@ -1,8 +1,9 @@
-<label class="block cursor-pointer">
+<label class="flex gap-2 cursor-pointer">
     <input
         {{ $attributes->class('form-boolean') }}
         type="checkbox"
         name="{{ $name }}"
-        {!! $checked !!}>
+        {!! $checked !!}
+    >
     <span class="align-middle select-none">{{ $slot ?? $label }}</span>
 </label>
