37b724ea900cadd77ffd678a861d1342e39c0990
Alexander Bondar <alexander@bondar.de>
2024-05-31 17:34:42 +0200

    Catch exceptions in the about command

--- src/Http/Controllers/Admin/System/AdminAboutController.php
+++ src/Http/Controllers/Admin/System/AdminAboutController.php
@@ -6,16 +6,26 @@ use BondarDe\Lox\LoxServiceProvider;
 use Illuminate\Contracts\View\View;
 use Illuminate\Foundation\Console\AboutCommand;
 use Illuminate\Support\Facades\Artisan;
+use Throwable;
 
 class AdminAboutController
 {
     public function __invoke(): View
     {
-        Artisan::call(AboutCommand::class, [
-            '--json' => 1,
-        ]);
-        $output = Artisan::output();
-        $systemStatus = json_decode($output);
+        try {
+            Artisan::call(AboutCommand::class, [
+                '--json' => 1,
+            ]);
+            $output = Artisan::output();
+            $systemStatus = json_decode($output);
+        } catch (Throwable $e) {
+            $systemStatus = [
+                'ERROR' => [
+                    'Exception' => $e::class,
+                    'message' => $e->getMessage(),
+                ],
+            ];
+        }
 
         return view(LoxServiceProvider::NAMESPACE . '::admin.system.about', compact(
             'systemStatus',
