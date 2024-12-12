afe03047d880127c92b4b179c88b25fe145c87b4
Alexander Bondar <alexander@bondar.de>
2024-12-12 17:05:40 +0100

    Reformat

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,9 @@ use BondarDe\Lox\Http\Controllers\Web\Sso\SsoCallbackController;
 use BondarDe\Lox\Http\Controllers\Web\Sso\SsoRedirectController;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Support\Facades\Route;
-use Laravel\Fortify\Features;
+use Laravel\Fortify\Features as FortifyFeatures;
 
-if (Features::enabled('sso')) {
+if (FortifyFeatures::enabled('sso')) {
     Route::group(['middleware' => ['web']], function () {
         Route::get('login/{provider}', SsoRedirectController::class)->name('sso.redirect');
         Route::any('login/{provider}/callback', SsoCallbackController::class)->name('sso.callback')
