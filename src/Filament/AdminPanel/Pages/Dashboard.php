c86165e9a7fca3ab742bf82c8acf41212cfb50be
Alexander Bondar <alexander@bondar.de>
2024-12-12 17:11:58 +0100

    Add admin dashboard page

---
+++ src/Filament/AdminPanel/Pages/Dashboard.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace BondarDe\Lox\Filament\AdminPanel\Pages;
+
+use Filament\Pages\Dashboard as BaseDashboard;
+
+class Dashboard extends BaseDashboard
+{
+    protected static ?string $navigationIcon = 'heroicon-o-squares-2x2';
+}
