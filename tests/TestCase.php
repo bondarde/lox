ee2073d4f597b8121f7f81deb78c1cbce9bad5d4
Alexander Bondar <alexander@bondar.de>
2024-12-13 17:28:28 +0100

    Fix test setup

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,7 +5,6 @@ namespace Tests;
 use BondarDe\Lox\LoxServiceProvider;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\ServiceProvider;
-use JetBrains\PhpStorm\NoReturn;
 use Livewire\LivewireServiceProvider;
 use Orchestra\Testbench\TestCase as BaseTestCase;
 
@@ -21,8 +20,7 @@ abstract class TestCase extends BaseTestCase
     /**
      * Get package providers.
      *
-     * @param Application $app
-     *
+     * @param  Application  $app
      * @return array<int, class-string<ServiceProvider>>
      */
     protected function getPackageProviders($app): array
@@ -33,17 +31,16 @@ abstract class TestCase extends BaseTestCase
         ];
     }
 
-    #[NoReturn]
     protected function setUpDatabase(): void
     {
         $migrationFiles = [
-            __DIR__ . '/../database/migrations/create_cms_pages_table.php',
-            __DIR__ . '/../database/migrations/create_cms_redirects_table.php',
+            __DIR__ . '/../database/migrations/001_create_cms_pages_table.php',
+            __DIR__ . '/../database/migrations/002_create_cms_redirects_table.php',
         ];
 
         foreach ($migrationFiles as $migrationFile) {
             $migrationClass = require $migrationFile;
-            (new $migrationClass)->up();
+            (new $migrationClass())->up();
         }
     }
 }
