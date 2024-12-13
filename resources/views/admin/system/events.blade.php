be6f44f0e1805b5d795f2419f2bcd2879c96afcd
Alexander Bondar <alexander@bondar.de>
2024-12-13 17:15:40 +0100

    Migrate events to Filament

--- resources/views/admin/system/events.blade.php
+++ resources/views/admin/system/events.blade.php
@@ -1,33 +1,7 @@
-<x-admin-page
-    title="Events"
-    h1="Events"
->
+<x-filament-panels::page>
 
-    <x-content class="overflow-x-auto">
-        <table class="table">
-            <thead>
-            <tr>
-                <th>Event</th>
-                <th>Listeners</th>
-            </tr>
-            </thead>
-            @foreach($events as $event => $listeners)
-                <tr class="border-t">
-                    <td>
-                        {{ $event }}
-                    </td>
-                    <td>
-                        <ul>
-                            @foreach($listeners as $listener)
-                                <li>
-                                    {{ $listener }}
-                                </li>
-                            @endforeach
-                        </ul>
-                    </td>
-                </tr>
-            @endforeach
-        </table>
-    </x-content>
+    <div>
+        {{ $this->table }}
+    </div>
 
-</x-admin-page>
+</x-filament-panels::page>
