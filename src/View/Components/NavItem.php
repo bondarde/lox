e2749e325624aa68c30ebf13fa4a9bc9e3b33cce
Alexander Bondar <alexander@bondar.de>
2024-12-18 13:19:29 +0100

    Allow making nav item active by path

--- src/View/Components/NavItem.php
+++ src/View/Components/NavItem.php
@@ -3,7 +3,9 @@
 namespace BondarDe\Lox\View\Components;
 
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Str;
 use Illuminate\View\Component;
 
 class NavItem extends Component
@@ -11,13 +13,20 @@ class NavItem extends Component
     public string $cssClasses;
 
     public function __construct(
-        public readonly string  $href,
-        string|array            $activeRoute,
+        Request $request,
+        public readonly string $href,
+        string|array|null $activeRoute = null,
+        string|array|null $activePath = null,
         public readonly ?string $info = null,
         public readonly ?string $containerClass = null,
-    )
-    {
-        $active = Route::is($activeRoute);
+    ) {
+        $active = false;
+        if ($activeRoute && Route::is($activeRoute)) {
+            $active = true;
+        }
+        if ($activePath && Str::is($activePath, $request->path())) {
+            $active = true;
+        }
 
         $baseClasses = 'relative after:absolute after:block after:h-1 after:-bottom-1 after:transition-all after:-skew-x-12';
         $hoverClasses = 'hover:after:left-0 hover:after:w-full hover:after:bg-nav-highlight';
