431d55ae6c5245f71542a9881f86e219112d9fd8
Alexander Bondar <alexander@bondar.de>
2024-12-18 13:46:13 +0100

    Add Filament CSS variables to HTML header

--- src/View/Components/HtmlHeader.php
+++ src/View/Components/HtmlHeader.php
@@ -2,6 +2,8 @@
 
 namespace BondarDe\Lox\View\Components;
 
+use Filament\Support\Assets\AssetManager;
+use Filament\Support\Facades\FilamentColor;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
@@ -11,13 +13,34 @@ class HtmlHeader extends Component
         readonly public ?string $title = null,
         readonly public ?string $shareImage = null,
         readonly public ?string $metaDescription = null,
-    )
+    ) {
+        //
+    }
+
+    private static function cssVariables(): string
     {
+        /** @var AssetManager $assetManager */
+        $assetManager = app(AssetManager::class);
+        $cssVariables = $assetManager->getCssVariables();
+
+        foreach (FilamentColor::getColors() as $name => $shades) {
+            foreach ($shades as $shade => $color) {
+                $cssVariables["{$name}-{$shade}"] = $color;
+            }
+        }
+
+        return view('filament::assets', [
+            'assets' => [],
+            'cssVariables' => $cssVariables,
+        ])->render();
     }
 
     public function render(): View
     {
+        $cssVariables = self::cssVariables();
+
         return view('lox::components.html-header', compact(
+            'cssVariables',
         ));
     }
 }
