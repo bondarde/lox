610d9fd19bda073e4aea501bdab13e42bfac8ec2
Alexander Bondar <alexander@bondar.de>
2025-01-12 17:47:50 +0100

    Update permissions check

--- src/Models/User.php
+++ src/Models/User.php
@@ -55,7 +55,10 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return $this->hasRole(Role::SuperAdmin)
-            || $this->hasPermissionTo('panel_' . $panel->getId());
+        $panelId = $panel->getId();
+
+        return $panelId === 'me'
+            || $this->hasRole(Role::SuperAdmin)
+            || $this->hasPermissionTo('panel_' . $panelId);
     }
 }
