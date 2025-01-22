763ea91010908bf1c593ec56aaf3cb5ac269ed66
Alexander Bondar <alexander@bondar.de>
2025-01-22 15:14:30 +0100

    Make first year optional in year filter

--- src/Filament/Tables/Filters/YearFilter.php
+++ src/Filament/Tables/Filters/YearFilter.php
@@ -10,20 +10,52 @@ class YearFilter
 {
     public static function make(
         string $field,
-        int $firstYear,
+        ?int $firstYear = null,
         ?int $lastYear = null,
+        ?string $filterName = null,
     ): SelectFilter {
-        $options = [];
-        $lastYear ??= Carbon::now()->year;
+        $filterName ??= $field . '.year';
+        $filter = SelectFilter::make($filterName);
 
+        return $filter
+            ->options(fn () => self::makeOptions($filter, $field, $firstYear, $lastYear))
+            ->query(
+                fn (Builder $query, array $state) => $state['value'] && $query->whereYear($field, $state['value']),
+            );
+    }
+
+    private static function makeOptions(
+        SelectFilter $filter,
+        string $field,
+        ?int $firstYear,
+        ?int $lastYear,
+    ): array {
+        $lastYear ??= Carbon::now()->year;
+        $firstYear ??= self::toFirstYear($filter, $field)
+            ?: $lastYear;
+
+        $options = [];
         for ($y = $lastYear; $y >= $firstYear; $y--) {
             $options[$y] = $y;
         }
 
-        return SelectFilter::make($field . '.year')
-            ->options($options)
-            ->query(
-                fn (Builder $query, array $state) => $state['value'] && $query->whereYear($field, $state['value']),
-            );
+        return $options;
+    }
+
+    private static function toFirstYear(
+        SelectFilter $filter,
+        string $field,
+    ): ?int {
+        $table = $filter->getTable();
+        $query = $table->getQuery();
+        $minValue = $query->min($field);
+
+        if ($minValue) {
+            $minDate = Carbon::parse($minValue);
+
+            return $minDate->year;
+        }
+
+        return null;
     }
 }
