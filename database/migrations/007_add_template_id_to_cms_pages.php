8200d6b05ffd0178a6f270d2313ddd5b194c4351
Alexander Bondar <alexander@bondar.de>
2025-01-29 00:55:17 +0100

    Fix migration

--- database/migrations/007_add_template_id_to_cms_pages.php
+++ database/migrations/007_add_template_id_to_cms_pages.php
@@ -1,15 +1,17 @@
 <?php
 
 use BondarDe\Lox\Models\CmsPage;
+use BondarDe\Lox\Models\CmsTemplate;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     public function up(): void
     {
         Schema::table('cms_pages', function (Blueprint $table) {
-            $table->foreignId(CmsPage::FIELD_CMS_TEMPLATE_ID)
+            $table->foreignIdFor(CmsTemplate::class)
                 ->nullable()
                 ->constrained();
         });
@@ -18,6 +20,8 @@ return new class extends Migration {
     public function down(): void
     {
         Schema::table('cms_pages', function (Blueprint $table) {
+            $table->dropForeignIdFor(CmsTemplate::class);
+
             $table->dropColumn(CmsPage::FIELD_CMS_TEMPLATE_ID);
         });
     }
