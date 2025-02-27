d0170ce95d8b601b49f857131f491bc9c4f69c31
Alexander Bondar <alexander@bondar.de>
2025-02-27 16:08:46 +0100

    Add date/datetime Filament components

---
+++ src/Filament/Infolists/Entries/DateTimeEntry.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace BondarDe\Lox\Filament\Infolists\Entries;
+
+use Carbon\Carbon;
+use Filament\Infolists\Components\TextEntry;
+
+class DateTimeEntry extends TextEntry
+{
+    public static function make(string $name): static
+    {
+        return parent::make($name)
+            ->formatStateUsing(
+                fn (Carbon $state) => $state->diffForHumans(),
+            )
+            ->helperText(
+                fn (Carbon $state) => $state->format('d.m.Y H:i'),
+            );
+    }
+}
---
+++ src/Filament/Tables/Columns/DateColumn.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace BondarDe\Lox\Filament\Tables\Columns;
+
+use Carbon\Carbon;
+use Filament\Tables\Columns\TextColumn;
+
+class DateColumn extends TextColumn
+{
+    public static function make(string $name): static
+    {
+        return parent::make($name)
+            ->formatStateUsing(
+                fn (Carbon $state) => $state->diffForHumans(),
+            )
+            ->description(
+                fn (Carbon $state) => $state->format('d.m.Y'),
+            );
+    }
+}
